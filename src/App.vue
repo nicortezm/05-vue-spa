3ba66df174ff7946ec6288a01a7e8e3968771f0e
nicortezm <nascorman@gmail.com>
2024-09-08 19:42:11 -0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <HomePage />
+</template>
+
+<script setup>
+import HomePage from '@/modules/landing/pages/HomePage.vue'
+
+</script>
+<style lang="scss" scoped></style>
---
+++ src/modules/landing/pages/FeaturesPage.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1 className="text-3xl font-bold underline">Features Page</h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
